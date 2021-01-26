d516ef29f2cb9c1d4930fb09dc431010065ad2fa
roberto <robpalacios1@hotmail.com>
2021-01-25 21:22:08 -0500

    fix code an put spreed movie

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-  const IMG_API = "https://image.tmdb.org/t/p/w1280";
-
-  const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 function App() {
 
@@ -25,7 +23,7 @@ function App() {
     <div className="App">
       {
         movies.length > 0 && movies.map(movie => (
-          <Movie key={movie.id} data={movie}/>
+          <Movie key={movie.id} {...movie}/>
         ))
       }
     </div>
