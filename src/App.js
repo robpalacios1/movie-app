392605b08b84494fe3e7f4ef7229f0f2cd5214de
roberto <robpalacios1@hotmail.com>
2021-01-25 19:48:16 -0500

    add useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Movie from './components/Movie'
 import './App.css';
 
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+  const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+  const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+
 function App() {
 
-  const movies = ['1','2','3','4'];
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      setMovies(data.results);
+    })
+  }, [])
 
   return (
     <div className="App">
       {
-        movies.map(movie => (
-          <Movie />
+        movies.length > 0 && movies.map(movie => (
+          <Movie key={movie.id} data={movie}/>
         ))
       }
     </div>
