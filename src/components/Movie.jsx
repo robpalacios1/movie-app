62a44419ecd84bbe766a839068d53ba4920f9da9
roberto <robpalacios1@hotmail.com>
2021-01-26 09:42:42 -0500

    add overview and change destructuring by props

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,13 +3,18 @@ import './Movie.css'
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-const Movie = ({ title, poster_path, overview, vote_average }) => {
+const Movie = (props) => {
     return (
         <div className="movie">
-            <img src={IMG_API + poster_path} alt={title}/>
+            <img src={IMG_API + props.poster_path} alt={props.title}/>
             <div className="movie-info">
-                <h3>{title}</h3>
-                <span>{vote_average}</span>
+                <h3>{props.title}</h3>
+                <span>{props.vote_average}</span>
+            </div>
+
+            <div className="movie-over">
+                <h2>Overview:</h2>
+                <p>{props.overview}</p>
             </div>
         </div>
     )
