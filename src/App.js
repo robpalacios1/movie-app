4378afce3d432cda0107ad0deb185174e962687a
roberto <robpalacios1@hotmail.com>
2021-01-26 11:57:19 -0500

    order code

--- src/App.js
+++ src/App.js
@@ -12,23 +12,23 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch(FEATURED_API)
+    getMovies(FEATURED_API)
+  }, [])
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data)
       setMovies(data.results);
     })
-  }, [])
+  }
 
   const handleOnSubmit = (event) => {
     event.preventDefault();
 
     if(searchTerm) {
-      fetch(SEARCH_API+searchTerm)
-        .then((res) => res.json())
-        .then((data) => {
-        setMovies(data.results);
-      })
+      getMovies(SEARCH_API + searchTerm)
       setSearchTerm('');
     }
   }
