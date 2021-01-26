728d9b9b70b8e8dd018820c2e57294987702ee8e
roberto <robpalacios1@hotmail.com>
2021-01-26 14:02:12 -0500

    add Footer.jsx component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h3>Made by Roberto Palacios</h3>
+            <p>2021</p>
+        </div>
+    )
+}
+
+export default Footer
