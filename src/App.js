9700d5e86a9be3cd11684535f92f24130dd1d053
roberto <robpalacios1@hotmail.com>
2021-01-26 14:08:54 -0500

    add comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, {
+  useState,
+  useEffect,
+  Fragment
+} from 'react'
+
+/***** Component *****/
 import Movie from './components/Movie'
 import Footer from './components/Footer'
+
+/***** CSS *****/
 import './App.css';
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 function App() {
 
   const [movies, setMovies] = useState([])
   const [searchTerm, setSearchTerm] = useState('');
+
+  const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+  const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
   useEffect(() => {
     getMovies(FEATURED_API)
