990f02718527aaa651e865acb0500c4cf5d4724a
roberto <robpalacios1@hotmail.com>
2021-01-25 21:54:28 -0500

    add class movie-info

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Movie.css'
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
@@ -6,6 +7,10 @@ const Movie = ({ title, poster_path, overview, vote_average }) => {
     return (
         <div className="movie">
             <img src={IMG_API + poster_path} alt={title}/>
+            <div className="movie-info">
+                <h3>{title}</h3>
+                <span>{vote_average}</span>
+            </div>
         </div>
     )
 }
