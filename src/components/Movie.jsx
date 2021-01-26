b96c610f113d32ac457461418482c776c28cc14d
roberto <robpalacios1@hotmail.com>
2021-01-25 21:21:08 -0500

    add props and add img tag to show it

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Movie = () => {
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, overview, vote_average }) => {
     return (
         <div className="movie">
-            <h1>hola</h1>
+            <img src={IMG_API + poster_path} alt={title}/>
         </div>
     )
 }
