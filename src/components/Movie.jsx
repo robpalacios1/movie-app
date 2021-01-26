a672a79bfc5dda8cb7fef90c332dd3deb747773e
roberto <robpalacios1@hotmail.com>
2021-01-26 13:43:51 -0500

    add image

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,12 +4,33 @@ import './Movie.css'
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const Movie = (props) => {
+
+    const setVoteClass = (vote) => {
+        if(vote >= 8) {
+            return 'green';
+        } else if (vote >= 6) {
+            return 'yellow';
+        } else {
+            return 'red';
+        }
+    }
+
     return (
         <div className="movie">
-            <img src={IMG_API + props.poster_path} alt={props.title}/>
+            <img
+                src={
+                    props.poster_path ? (
+                        IMG_API + props.poster_path
+                        ) : (
+                            'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=eyJhcHHfaWQiOjEyMDd9&auto=format&fit=crop&1480&q=80'
+                        )
+                } alt={props.title}/>
             <div className="movie-info">
                 <h3>{props.title}</h3>
-                <span>{props.vote_average}</span>
+                <span
+                    className={`tag ${setVoteClass(props.vote_average)}`}
+                >{props.vote_average}
+                </span>
             </div>
 
             <div className="movie-over">
